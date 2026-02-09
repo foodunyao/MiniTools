3c6d6bf8468ceb12b80bbb63ac86a33a7ce0facf
Dun Yao <foodunyao@gmail.com>
2026-02-09 23:43:51 +1100

    feat: included header and started on sidebar logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,23 @@
 import { useState } from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  const toggleSidebar = () => {
+    if (showSidebar) {
+      setShowSidebar(false);
+    } else {
+      setShowSidebar(true);
+    }
+  };
 
   return (
     <>
+      <Header toggleSidebar={toggleSidebar}></Header>
+      {showSidebar ? <p>SIDEBARSHOWN</p> : <p>SIDEBARHIDDEN</p>}
       <p>{count}</p>
       <button onClick={() => setCount((prev) => (prev += 1))}>Increment</button>
     </>
